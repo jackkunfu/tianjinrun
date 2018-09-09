61cdf6e4e45831dc309cbbda1f72476017418612
lixiaoyu <lxy@hpaopao.com>
2018-09-09 17:15:16 +0830

    更改样式

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 export default {
-//    baseUrl: 'http://ceshi.marathonbm.com',
-    baseUrl: 'http://192.168.2.13:8082'
+   baseUrl: 'http://ceshi.marathonbm.com',
+    // baseUrl: 'http://192.168.2.13:8082'
 }
